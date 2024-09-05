4c2ddafe93544fe841fa255d477a62d6e545a23f
Gayan Perera <mgdperera@yahoo.com>
2024-09-05 14:53:19 +0530

    add coments for play and pause video

--- src/componet/videoPlayer/index.js
+++ src/componet/videoPlayer/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactPlayer from 'react-player'
 
 export default function VideoPlayer() {
+    const [playing, setPlaying] = useState(true); // play and pause the video
+
+
     const handleProgress = (progress) => {
-        console.log('Played: ', progress.played , ' Played Seconds:', progress.playedSeconds, ' Loaded Seconds:', progress.loadedSeconds); // Percentage of video played
-      };
+        console.log('IsPlaying: ',playing,' Played: ', progress.played , ' Played Seconds:', progress.playedSeconds, ' Loaded Seconds:', progress.loadedSeconds); // Percentage of video played
+       
+    //     if(progress.playedSeconds > 6 ){ // 6s is the place that questions is comming
+    //      setPlaying(false)
+    //    }  
+    };
 
   return (
     <div>VideoPlayer12
@@ -13,7 +20,7 @@ export default function VideoPlayer() {
       controls={false}
       height="60%"
       width="10%"
-      playing={true}
+      playing={playing} //deafult it's false
       muted = {true}
       onError={e => console.log('onError [ReactPlayer]', e)}
       onProgress={handleProgress}
