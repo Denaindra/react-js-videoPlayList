b2025e85abada374c12ea34252a4f3c916805da0
Gayan Perera <mgdperera@yahoo.com>
2024-09-18 08:37:50 +0530

    add full screen layout video

--- src/componet/videoPlayer/index.js
+++ src/componet/videoPlayer/index.js
@@ -1,30 +1,50 @@
-import React, { useState } from 'react'
-import ReactPlayer from 'react-player'
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import "./videoPlayer.css";
 
 export default function VideoPlayer() {
-    const [playing, setPlaying] = useState(true); // play and pause the video
+  const [playing, setPlaying] = useState(true); // play and pause the video
 
-
-    const handleProgress = (progress) => {
-        console.log('IsPlaying: ',playing,' Played: ', progress.played , ' Played Seconds:', progress.playedSeconds, ' Loaded Seconds:', progress.loadedSeconds); // Percentage of video played
-       
-        if(progress.playedSeconds > 1.4 ){ // 6s is the place that questions is comming
-         setPlaying(false)
-       }  
+  useEffect(() => {
+    // Disable scrolling when the component is mounted
+    document.body.style.overflow = "hidden";
+    return () => {
+      // Enable scrolling when the component is unmounted
+      document.body.style.overflow = "auto";
     };
+  }, []);
+
+  const handleProgress = (progress) => {
+    console.log(
+      "IsPlaying: ",
+      playing,
+      " Played: ",
+      progress.played,
+      " Played Seconds:",
+      progress.playedSeconds,
+      " Loaded Seconds:",
+      progress.loadedSeconds
+    ); // Percentage of video played
+
+    if (progress.playedSeconds > 1.4) {
+      // 6s is the place that questions is comming
+      setPlaying(false);
+    }
+  };
 
   return (
-    <div>VideoPlayer12
-        <ReactPlayer 
-      url={'assets/videoAssets/0921.mov'}
-      controls={true}
-      height="60%"
-      width="10%"
-      playing={playing} //deafult it's false
-      muted = {true}
-      onError={e => console.log('onError [ReactPlayer]', e)}
-      onProgress={handleProgress}
-    />
+    <div> 
+        <ReactPlayer
+          url={"assets/videoAssets/0921.mov"}
+          controls={true}
+          height="100vh"
+          width="100vw"
+          className="react-player" 
+          playing={playing} //deafult it's false
+          muted={true}
+          onError={(e) => console.log("onError [ReactPlayer]", e)}
+          onProgress={handleProgress}
+        />
     </div>
-  )
+  );
 }
